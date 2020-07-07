cce9ecfd850a1fd0ce1bf39671d128767255c2e0
alswell <icemanzhouning@yeah.net>
2020-07-07 12:53:31 +0800

    copy link to clipboard

--- bookmarks/popup.js
+++ bookmarks/popup.js
@@ -1,4 +1,29 @@
-document.getElementById("x").onclick=function() {
+document.getElementById("dump").onclick=function() {
 	chrome.tabs.create({url: "bookmarks.html"})
 };
+
+document.getElementById("load").onclick=function() {
+	chrome.tabs.create({url: "load.html"})
+};
+
+document.getElementById("copy").onclick=function() {
+	chrome.tabs.getSelected(null, function(tab) {
+		setClipBoardData(`[${tab.title}](${tab.url})`)
+	});
+};
+
+
+function setClipBoardData(text) {
+	const copy = function (e) {
+		e.preventDefault();
+		if (e.clipboardData) {
+			e.clipboardData.setData('text/plain', text);
+		} else if (window.clipboardData) {
+			window.clipboardData.setData('Text', text);
+		}
+	};
+	window.addEventListener('copy', copy);
+    document.execCommand('copy');
+    window.removeEventListener('copy', copy);
+}
 
